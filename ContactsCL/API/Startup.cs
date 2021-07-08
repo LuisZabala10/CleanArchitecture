dbad101741b9a38a4b709a3729246cf3e28a88db
Luis Zabala <zabalaluis1234@gmail.com>
2021-07-08 19:43:42 -0300

    Agregando soporte para xml formatters.

--- ContactsCL/API/Startup.cs
+++ ContactsCL/API/Startup.cs
@@ -34,7 +34,10 @@ namespace API
 
             services.AddDbContext<ContactsDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("db")));
 
-            services.AddControllers();
+            services.AddControllers(config=> {
+                config.RespectBrowserAcceptHeader = true;
+                config.ReturnHttpNotAcceptable = true;
+            }).AddXmlDataContractSerializerFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
